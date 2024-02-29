bbc29c440830193fac343136d3f60bef3b39cd0f
Aijem <aijemalmacin2018@gmail.com>
2024-02-29 17:57:02 +0800

    third commmit

--- MauiBlazor/obj/Debug/net8.0-ios/iossimulator-x64/MauiBlazor.AssemblyInfo.cs
+++ MauiBlazor/obj/Debug/net8.0-ios/iossimulator-x64/MauiBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ac6a1c6da6763a0d7c5ac8dace5c7a0ea79992dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
